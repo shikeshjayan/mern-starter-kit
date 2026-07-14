51b86f43f3a99a508367db98363219656af4f896
shikeshjayan <shikeshjayan345@gmail.com>
2026-07-14 11:51:39 +0530

    Setup MongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
 
 // Load environment variables
 dotenv.config();
+
+// Connect to the database
+await connectDB();
 
 // Start the server
 const PORT = process.env.PORT || 5000;
